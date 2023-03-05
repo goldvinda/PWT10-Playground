22fc513682905204a0ff0d9137bc924a795d8c6d
goldvinda <juanrgaudin@gmail.com>
2023-03-05 14:22:06 +0000

    v0.3.1

--- preliminaires.py
+++ preliminaires.py
@@ -1,2 +1,4 @@
 import pandas as pd
-pwt = pd.read_excel('pwt1001.xlsx', sheetname="Data")
+pwt = pd.read_excel('pwt1001.xlsx', sheet_name="Data")
+pwt_subset = pwt[['country','countrycode','year','cgdpo','pop','emp','hc','cn','labsh','ctfp']]
+pwt_subset = pwt_subset[pwt_subset['year'] > 2009].set_index('countrycode').dropna()
